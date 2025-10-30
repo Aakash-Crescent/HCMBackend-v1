5a920e9a24fc3b48bd9f29de9e05757a727174c7
Aakash Goradia <agoradia@crescentpharma.com>
2025-10-30 12:05:45 +0530

    Allowed Cookie Access for Internal Re-directs: strict -> lax

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -33,7 +33,8 @@ export const loginUser = async (req: Request, res: Response) => {
     res.cookie("auth_token", token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production",
-      sameSite: "strict",
+      sameSite: "lax",
+      path: "/",
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
 
